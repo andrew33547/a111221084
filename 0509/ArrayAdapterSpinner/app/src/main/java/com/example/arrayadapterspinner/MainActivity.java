33e7bdd9b645cab381ce7363706c42be249048e8
andrew33547 <andrew2004090104@gmail.com>
2024-05-09 18:03:33 +0800

    4

---
+++ 0509/ArrayAdapterSpinner/app/src/main/java/com/example/arrayadapterspinner/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.arrayadapterspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner drink, temp;
+    private TextView txv;
+    private String[] tempSet1 = {"冰", "去冰", "燙"};
+    private String[] tempSet2 = {"冰", "去冰"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txv = (TextView) findViewById(R.id.order);
+        temp = (Spinner) findViewById(R.id.temp);
+
+        drink = (Spinner) findViewById(R.id.drink);
+        drink.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        }
+
+    }
+}
